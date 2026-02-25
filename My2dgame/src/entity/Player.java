8a335ce39b718191fd428de761ab46b973869da0
qeemsantiago <qeemsantiago@tip.edu.ph>
2026-02-25 23:55:01 +0800

    my 2d game

---
+++ My2dgame/src/entity/Player.java
@@ -0,0 +1,94 @@
+package entity;
+
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+public class Player extends Entity {
+
+	GamePanel gp;
+	KeyHandler keyH;
+	
+	public Player(GamePanel gp, KeyHandler keyH) {
+		
+		this.gp = gp;
+		this.keyH = keyH;
+		
+		setDefaultValues();
+		getPlayerImage();
+	}
+	public void setDefaultValues() {
+		
+		
+		x = 100;
+		y = 100;
+		speed = 4;
+		direction = "up";
+		
+	}
+	public void getPlayerImage() {
+		
+		try {
+			
+			up = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			down = ImageIO.read(getClass().getResourceAsStream("/player/boy_down.png"));
+		    left = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+			right = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+			
+			
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update() {
+		
+		if(keyH.upPressed == true) {
+			direction = "up";
+			y -= speed; 
+		}
+		else if(keyH.downPressed == true) {
+			direction = "down";
+			y += speed; 
+		}
+		else if(keyH.leftPressed == true) {
+			direction = "left";
+			x -= speed;
+		}
+		else if(keyH.rightPressed == true) {
+			direction = "right";
+			x+= speed;
+		}
+	}
+	public void draw(Graphics2D g2) {
+		
+      // g2.setColor(Color.white);
+		
+	//g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+		
+		BufferedImage image = null;
+		
+		switch(direction) {
+		case "up":
+			image = up;
+			break;
+		case "down":
+			image = down;
+			break;
+		case "left":
+			image = left;
+			break;
+		case "right":
+			image = right;
+			break;
+		}
+		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+	}
+}
---
+++ My2dgame/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		window.setTitle("Unreliable Narrator");
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gamePanel.starGameThread();
+		
+	}
+ 
+}
